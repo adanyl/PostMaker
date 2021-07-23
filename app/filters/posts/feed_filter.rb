8459bb13825c6747029f96ec4133104fa8479a49
Oleksii <o.danylenko@mobidev.biz>
2021-07-23 15:13:15 +0300

    Edited solution of the second task

--- app/filters/posts/feed_filter.rb
+++ app/filters/posts/feed_filter.rb
@@ -1,19 +1,15 @@
 module Posts
   class FeedFilter < BaseFilter
-
-
-
-    
     def perform
-      if input[:search_text].nil? && input[:social_networks].nil? && input[:date_from].nil? && input[:date_to].nil?
-        base_scope 
+      if input[:search_text].present? || input[:social_networks].present? || input[:date_from].present? || input[:date_to].present?
+        scope = base_scope
+        scope = scope.where("title like ? or post like ?", "%" + input[:search_text] + "%", "%" + input[:search_text] + "%") if input[:search_text].present?
+        scope = scope.where('social_network IN (?)', input[:social_networks]).references(:social_posts) if input[:social_networks].present?
+        scope = scope.where('schedule_time >= ?', input[:date_from]).references(:social_posts) if input[:date_from].present?
+        scope = scope.where('schedule_time <= ?', input[:date_to]).references(:social_posts) if input[:date_to].present?
+        scope
       else
-        
-      scope = base_scope
-      scope = scope.where("title like ? or post like ?", "%" + input[:search_text] + "%", "%" + input[:search_text] + "%") if input[:search_text].present?
-      scope = scope.where('social_network IN (?)', input[:social_networks]).references(:social_posts) if input[:social_networks].present?
-      scope = scope.where('schedule_time >= ?', input[:date_from]).references(:social_posts) if input[:date_from].present?
-      scope = scope.where('schedule_time <= ?', input[:date_to]).references(:social_posts) if input[:date_to].present?
+        base_scope
       end
     end
     private
